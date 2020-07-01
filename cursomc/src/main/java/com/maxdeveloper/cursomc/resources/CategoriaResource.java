ae6922e5f5a0f02c8ffda19ee871596a23409cef
maxsaldanha <maxuelsaldanha@gmail.com>
2020-07-01 20:30:43 -0300

    Filtrando a categoria

--- cursomc/src/main/java/com/maxdeveloper/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/maxdeveloper/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.maxdeveloper.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maxdeveloper.cursomc.domain.Categoria;
+import com.maxdeveloper.cursomc.dto.CategoriaDTO;
 import com.maxdeveloper.cursomc.services.CategoriaService;
 
 @RestController
@@ -54,4 +57,11 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();	
 		
 	}
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list= service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());						
+		return ResponseEntity.ok().body(listDto);
 	}
+}
